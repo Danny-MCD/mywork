e00765f7f8074cac3f9076b362ee137a8a296dc6
Daniel Mc Donagh <g00410864@atu.ie>
2023-02-06 23:08:23 +0000

    extra work

---
+++ Week002/nameAndAge.py
@@ -0,0 +1,11 @@
+# nameAndAge.py
+# prints name and age from input
+# Author: Daniel MC Donagh
+
+
+name = input ("What is your name?")
+age = input ("and what is your age?")
+
+# \t used to add tab spacing
+
+print (f"hi {name},\t  you are {age}")
