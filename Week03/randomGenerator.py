43b0068e58c641fbade385d428eed7e7871d008d
Daniel Mc Donagh <g00410864@atu.ie>
2023-02-11 22:33:00 +0000

    randomGenerator version to take input of range

--- Week03/randomGenerator.py
+++ Week03/randomGenerator.py
@@ -1,4 +1,16 @@
 # program that prints out a random number between 1 and 10
+# version to take input of range from user
 import random
-number = random.randint(1,10)
+
+x = int(input("Enter first number in range: "))
+y = int(input("Enter second number in range: "))
+
+
+
+
+number = random.randint(x,y)
+
+
+
+
 print("here is a random number {}".format(number))
